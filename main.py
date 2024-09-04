7cd47a8cdce0a06460ea9542fd9e7a0ca495f841
aamina_akbar <66160761+aamina0093@users.noreply.github.com>
2024-09-04 22:43:08 +0530

    main.py

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
-import time
-import os
-from datetime import datetime, time as dt_time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from dotenv import load_dotenv
+from selenium.webdriver.chrome.service import Service
+import time
 
-print(time.ctime())
-site_url = os.environ.get("URL2")
-load_dotenv()
-field1 = os.environ.get('FIELD1')
-field2 = os.environ.get('FIELD2')
-field3 = os.environ.get('FIELD3')
-field4 = os.environ.get('FIELD4')
-field5 = os.environ.get('FIELD5')
+s = Service('C:\BrowserDrivers\chromedriver-win64\chromedriver-win64\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
 
-chrome_browser = webdriver.Chrome()
+# Open the Amazon login page
+driver.get("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com"
+           "%2Fcustomer-preferences%2Fedit%3Fie%3DUTF8%26preferencesReturnUrl%3D%252F%26ref_%3Dnav_signin&openid"
+           ".identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex"
+           "&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0"
+           "%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
 
-script_done = False
-while not script_done:
-    chrome_browser.get(site_url)
-    document_type = chrome_browser.find_element(By.ID, field1)
-    document_type.click()
-    document_type.send_keys(Keys.ARROW_DOWN)
-    document_type.send_keys(Keys.ENTER)
-    time.sleep(3)
-    document_number = chrome_browser.find_element(By.ID, field2)
-    document_number.send_keys("112345")
-    time.sleep(3)
-    document_issue_place = chrome_browser.find_element(By.ID, field3)
-    document_issue_place.send_keys("Bogota")
-    time.sleep(3)
-    document_issue_date = chrome_browser.find_element(By.ID, field4)
-    document_issue_date.click()
-    document_issue_date.send_keys("02045677")
-    time.sleep(3)
-    birth_date = chrome_browser.find_element(By.ID, field5)
-    birth_date.click()
-    birth_date.send_keys("01012000")
-    time.sleep(3)
-    script_done = True
 
-input("Press ENTER to close the browser....")
+# enter email
+user_email = driver.find_element(By.ID, "ap_email")
+user_email.send_keys("aamina@email.com")
+
+time.sleep(10)
+
+# select continue button
+continue_button = driver.find_element(By.ID, "continue")
+continue_button.click()
+
+time.sleep(10)
+
+# enter password
+user_password = driver.find_element(By.ID, "ap_password")
+user_password.send_keys("Aamina@121")
+
+time.sleep(10)
+
+# Click the 'Sign In' button
+sign_in_button = driver.find_element(By.ID, "signInSubmit")
+sign_in_button.click()
+
+# to print sign in status
+print(driver.title)
+
+# Close the WebDriver
+driver.quit()
+
